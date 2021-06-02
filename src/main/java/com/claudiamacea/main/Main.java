3a26fc14c7e40bd6178d4297d4b56a7dc8a7bad7
claudiamacea <66469512+claudiamacea@users.noreply.github.com>
2021-06-03 00:03:03 +0300

    commit3

--- src/main/java/com/claudiamacea/main/Main.java
+++ src/main/java/com/claudiamacea/main/Main.java
@@ -45,5 +45,6 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("commit1");
         System.out.println("commit2");
+        System.out.println("commit3");
     }
 }
