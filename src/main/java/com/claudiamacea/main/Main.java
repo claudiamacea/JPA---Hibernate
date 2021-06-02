8c2f46c9591a6fb99e0efc6618045a5f2dff77e5
claudiamacea <66469512+claudiamacea@users.noreply.github.com>
2021-06-03 00:06:23 +0300

    commit4

--- src/main/java/com/claudiamacea/main/Main.java
+++ src/main/java/com/claudiamacea/main/Main.java
@@ -46,5 +46,6 @@ public class Main {
         System.out.println("commit1");
         System.out.println("commit2");
         System.out.println("commit3");
+        System.out.println("commit4");
     }
 }
